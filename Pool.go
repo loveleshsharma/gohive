22b40f870bcf25b30b6eb5be3518c681afd9b292
Lovelesh Sharma <loveleshsharma@in-lovelesh.lan>
2019-06-16 19:02:13 +0530

    Added close pool feature and error handling for invalid pool size

--- Pool.go
+++ Pool.go
@@ -1,33 +1,62 @@
 package GoHive
 
+import (
+	"github.com/pkg/errors"
+)
+
+type state int
+
+const (
+	OPEN   state = 1
+	CLOSED state = 0
+)
+
+var (
+	ErrInvalidPoolState = errors.New("Pool is Closed: Cannot Assign task to a closed pool!")
+)
+
 type Pool struct {
 	capacity       int
 	runningWorkers int
 	workers        []Worker
-	routineService *RoutineService  //reference back to the routine service
+	status         state
+	routineService *RoutineService //reference back to the routine service who owns this pool
 }
 
 func NewFixedSizePool(newSize int, routineService *RoutineService) Pool {
-	newPool := Pool{capacity: newSize, workers: make([]Worker, newSize), routineService: routineService}
+	newPool := Pool{capacity: newSize, workers: make([]Worker, newSize), routineService: routineService, status: OPEN}
 	return newPool
 }
 
-func (p *Pool) assignTask(task Task) {
-	for i := 0; i < p.capacity; i++ {
-		if p.workers[i].isOccupied == false {
-			p.workers[i] = Worker{taskChan: make(chan func()),pool:p,isOccupied:true}
-			go p.workers[i].run()
-			p.workers[i].taskChan <- task.getTask()
-			p.runningWorkers++
-			break
+func (p *Pool) assignTask(task Task) error {
+	if p.status == OPEN {
+		for i := range p.workers {
+			if p.workers[i].isOccupied == false {
+				p.workers[i] = Worker{taskChan: make(chan func()), pool: p, isOccupied: true}
+				go p.workers[i].run()
+				p.workers[i].taskChan <- task.getTask()
+				p.runningWorkers++
+				break
+			}
 		}
+		return nil
 	}
+	return ErrInvalidPoolState
 }
 
 func (p *Pool) Done(w *Worker) {
 	p.runningWorkers--
 	w.isOccupied = false
-	p.routineService.notify() 	//	notify the RoutineService to pull more tasks from the queue if waiting
+	p.routineService.notify() //	notify the RoutineService to pull more tasks from the queue if waiting
+}
+
+func (p *Pool) Close() {
+	for i := range p.workers {
+		if p.workers[i].isOccupied == false {
+			p.workers[i] = Worker{}
+		}
+	}
+	p.status = CLOSED
 }
 
 func (p *Pool) isWorkerAvailable() bool {
--- RoutineService.go
+++ RoutineService.go
@@ -1,8 +1,15 @@
 package GoHive
 
-import "fmt"
+import (
+	"fmt"
+	"github.com/pkg/errors"
+)
 
 const DEFAULT_POOL_CAPACITY = 3
+
+var (
+	ErrInvalidPoolSize = errors.New("Invalid Pool Size: Pool Size must be a positive number!")
+)
 
 type RoutineService struct {
 	routinePool  Pool
@@ -10,33 +17,42 @@ type RoutineService struct {
 	poolSize     int
 }
 
-func NewDefaultRoutinePool() RoutineService {
+func NewDefaultRoutinePool() *RoutineService {
 	wtQueue := NewWaitingQueue()
 	routineService := RoutineService{waitingQueue: &wtQueue, poolSize: DEFAULT_POOL_CAPACITY}
-	pool := NewFixedSizePool(DEFAULT_POOL_CAPACITY,&routineService)
+	pool := NewFixedSizePool(DEFAULT_POOL_CAPACITY, &routineService)
 	routineService.routinePool = pool
-	return routineService
+	return &routineService
 }
 
-func NewFixedSizeRoutinePool(numOfRoutines int) RoutineService {
+func NewFixedSizeRoutinePool(numOfRoutines int) (*RoutineService, error) {
+
+	if numOfRoutines <= 0 {
+		panic(ErrInvalidPoolSize)
+	}
+
 	wtQueue := NewWaitingQueue()
 	routineService := RoutineService{waitingQueue: &wtQueue, poolSize: numOfRoutines}
-	pool := NewFixedSizePool(numOfRoutines,&routineService)
+	pool := NewFixedSizePool(numOfRoutines, &routineService)
 	routineService.routinePool = pool
-	return routineService
+	return &routineService, nil
 }
 
-func (rs *RoutineService) Submit(fun func()) {
+func (rs *RoutineService) Submit(fun func()) error {
 	newTask := Task{executable: fun}
 
 	//	if worker is available, immediately assigning the task
 	if rs.routinePool.isWorkerAvailable() {
 		fmt.Println("Assigning!")
-		rs.routinePool.assignTask(newTask)
+		err := rs.routinePool.assignTask(newTask)
+		if err != nil {
+			panic(err)
+		}
 	} else {
 		fmt.Println("Queuing!")
 		rs.waitingQueue.EnqueueTask(newTask)
 	}
+	return nil
 }
 
 //notifies the routineService that one of the worker from
@@ -46,7 +62,7 @@ func (rs *RoutineService) notify() {
 	if rs.waitingQueue.IsNotEmpty() {
 		task, err := rs.waitingQueue.DequeueTask()
 		if err != nil {
-			fmt.Println("Error Dequeueing Task!")
+			fmt.Errorf("Error Dequeueing Task!")
 			return
 		}
 		fmt.Println("Pulling task from Queue!")
@@ -64,4 +80,8 @@ func (rs *RoutineService) PoolCapacity() int {
 
 func (rs *RoutineService) AvailableWorkers() int {
 	return rs.routinePool.capacity - rs.routinePool.runningWorkers
+}
+
+func (rs *RoutineService) Close() {
+	rs.routinePool.Close()
 }
